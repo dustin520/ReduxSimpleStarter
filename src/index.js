30b8794341ccc0999621b0fad65ae585fa61fd0d
Dustin Yu <dustinyu@Dustin-MBA.local>
2017-10-24 15:26:24 -0700

    Fixed bug

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
+// Create new component, should produce html
 const App = function() {
    return <div>Hi!</div>;
  }
 
-
-React.render(App);
+// Take component's HTML and put in DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
